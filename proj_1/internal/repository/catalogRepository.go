8c6db5af3b87285941fefd5f5dea5c7306735d0e
phongluong8901 <phongluong3366@gmail.com>
2026-08-26 16:53:36 +0700

    change: add repository category

--- proj_1/internal/repository/catalogRepository.go
+++ proj_1/internal/repository/catalogRepository.go
@@ -1,19 +1,77 @@
 package repository
 
 import (
+	"errors"
+	"log"
+	"proj_1/internal/domain"
+
 	"gorm.io/gorm"
 )
 
 type CatalogRepository interface {
+	CreateCategory(e *domain.Category) error
+	FindCategories() ([]*domain.Category, error)
+	FindCategoryById(id int) (*domain.Category, error)
+	EditCategory(e *domain.Category) (*domain.Category, error)
+	DeleteCategory(id int) error
 }
 
 type catalogRepository struct {
 	db *gorm.DB
 }
 
-// constructors
+func (c catalogRepository) CreateCategory(e *domain.Category) error {
+	err := c.db.Create(&e).Error
+	if err != nil {
+		log.Printf("db_err: %v", err)
+		return errors.New("create category failed")
+	}
+	return nil
+}
+
 func NewCatalogRepository(db *gorm.DB) CatalogRepository {
 	return &catalogRepository{
 		db: db,
 	}
+}
+
+func (c catalogRepository) FindCategories() ([]*domain.Category, error) {
+	var categories []*domain.Category
+
+	err := c.db.Find(&categories).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return categories, nil
+}
+
+func (c catalogRepository) FindCategoryById(id int) (*domain.Category, error) {
+	var category *domain.Category
+
+	err := c.db.First(&category, id).Error
+	if err != nil {
+		log.Printf("db_err: %v", err)
+		return nil, errors.New("category doese not exist")
+	}
+
+	return category, nil
+}
+
+func (c catalogRepository) EditCategory(e *domain.Category) (*domain.Category, error) {
+	err := c.db.Save(&e).Error
+	if err != nil {
+		log.Printf("db_err: %v", err)
+		return nil, errors.New("fail to update category")
+	}
+	return e, nil
+}
+
+func (c catalogRepository) DeleteCategory(id int) error {
+	err := c.db.Delete(&domain.Category{}, id).Error
+	if err != nil {
+		log.Printf("db_err: %v", err)
+		return errors.New("fail to delete category")
+	}
+	return nil
 }
